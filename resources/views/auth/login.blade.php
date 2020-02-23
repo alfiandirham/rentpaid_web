929c239cb648a85df2e9568e082dbb0322accdf6
Suarmin Rais <suarmin.rais@gmail.com>
2020-02-23 08:08:20 +0800

    fix responsive layout login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,86 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-{{-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> --}}
-
-<div class="main-container container-fluid">
+<div class="main-container container-fluid d-none d-md-block">
     <div class="row">
         <div class="col-md-2">
             <div class="one-layer">
                 <div class="layer-one flex-column justify-content-center align-content-center">
-                    <div class="form-group">
+                    <div class="form-group-layer">
                         <h2 class="layer-title text-center">Masuk Akun</h2>
-                        <form action="" method="POST" class="form">
-                            <label class="layer-label" for="username">Username</label>
-                            <input class="form-control layer-form" placeholder="Masukan username atau no hp" name="username" type="text">
+                        <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+                        @csrf
+                            <label class="layer-label" for="username">Email</label>
+                            <input type="email" class="form-control layer-form{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                            @if ($errors->has('email'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
                             <label for="password" class="layer-label mt-4">Password</label>
-                            <input type="password" placeholder="Masukan password" class="layer-form form-control">
+                            <input type="password" placeholder="Masukan password" class="layer-form form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                            @if ($errors->has('password'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
                             <a href="#" class="btn mt-3 float-right forget-pass">Lupa Password ?</a>
                             <br>
                             <input type="submit" class="btn mt-3 btn-rentpaid" value="Masuk Akun">
@@ -89,14 +33,48 @@
                 </div>
             </div>
         </div>
-        <div class="layer-two"></div>
-        <div class="float-right">
-                <div class=" float-right container col-md-9">
-                    <div class="main-logo">
-                        <img src="{{asset('img/logo 2.png')}}" class="img-fluid" alt="">
-                        <img class="img-fluid pt-5" src="{{asset('img/payment_illustration.png')}}" alt="">
-                    </div>
-                </div>
+        <div class="layer-two d-none d-lg-block"></div>
+        <div class="container col-md-8 offset-2">
+            <div class="main-logo">
+                <img src="{{asset('img/logo 2.png')}}" class="img-fluid" alt="">
+                <img class="img-fluid pt-5" src="{{asset('img/payment_illustration.png')}}" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="d-block main-container-sm d-sm-none">
+    <div class="container row-sm">
+        <div class="logo-sm">
+            <img src="{{asset('img/logo 2.png')}}" class="img-fluid" alt="">
+        </div>
+    </div>
+    <div class="layer-two-sm"></div>
+    <div class="bg-rentpaid">
+        <div class="container row-sm layer-sm-one">
+            <div class="form-group">
+                <h2 class="layer-title text-center">Masuk Akun</h2>
+                <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+                @csrf
+                    <label class="layer-label" for="username">Email</label>
+                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                    @if ($errors->has('email'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                    <label for="password" class="layer-label mt-4">Password</label>
+                    <input id="password" type="password" placeholder="Masukan password" class="layer-form form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                    @if ($errors->has('password'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                    <a href="#" class="btn mt-3 float-right forget-pass">Lupa Password ?</a>
+                    <br>
+                    <input type="submit" class="btn mt-3 btn-rentpaid" value="Masuk Akun">
+                </form>
+            </div>
         </div>
     </div>
 </div>
